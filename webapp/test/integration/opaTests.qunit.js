ead6aca1e3c047237af44eccee47e90944d461cf
Manuel Pedro <manuel.pedro@dbtrust.com.ar>
2024-08-29 15:59:23 +0000

    Primer commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["dbtrust/invoices/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"dbtrust/invoices/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
