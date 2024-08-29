3b6aad4aa2ff92d0b96303f1d78dbaee4a1d8888
Manuel Pedro <manuel.pedro@dbtrust.com.ar>
2024-08-29 17:42:27 +0000

    Video 10

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,9 +1,14 @@
+//La funcion sap.ui.define tiene dos argumentos.
+//El primero es un array con dependencias para incorporar en la instancia del controlador
+//El segundo  es el codigo con la función que devuelve la instancia del controlador
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
+//La funcion recibe como parámetro el Controller que se cargó en la dependencia
 function (Controller) {
     "use strict";
 
+    //Dentro de onInit va la lógica
     return Controller.extend("dbtrust.invoices.controller.MainView", {
         onInit: function () {
 
